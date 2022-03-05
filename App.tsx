80d37b95dd1a6d6a7d2f858d303727fd1d784068
clickspider <danielfrey101@gmail.com>
2022-03-05 23:07:12 +0100

    Updated folder stracture 📂

--- App.tsx
+++ App.tsx
@@ -13,47 +13,14 @@ import {
   SafeAreaView,
   ScrollView,
   StatusBar,
-  StyleSheet,
-  Text,
   useColorScheme,
-  View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import MainHeader from './src/components/Main/MainHeader';
+
+import {View, Text, Card} from 'react-native-ui-lib';
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -68,48 +35,42 @@ const App = () => {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
+        <MainHeader />
+
         <View
+          flex
+          padding-page
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          <Text heading marginB-s4>
+            Hello Daniel
+          </Text>
+          <Card
+            style={{
+              marginBottom: 10,
+              backgroundColor: isDarkMode ? Colors.black : Colors.white,
+            }}
+            onPress={() => {}}>
+            <Card.Section
+              bg-white
+              content={[
+                {text: 'You’re Invited!', text70: true, grey10: true},
+                {text: 'join now', text90: true, grey50: true},
+              ]}
+              style={{padding: 20}}
+            />
+            <Card.Section
+              imageSource={{
+                uri: 'https://github.com/wix/react-native-ui-lib/blob/master/demo/src/assets/images/card-example.jpg?raw=true',
+              }}
+              imageStyle={{height: 120}}
+            />
+          </Card>
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
